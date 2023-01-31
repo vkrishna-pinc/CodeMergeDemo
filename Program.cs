f7923dc4087b5de582f69dcea9af2c182831fc2e
vkrishna-pinc <117730568+vkrishna-pinc@users.noreply.github.com>
2023-01-31 20:02:34 +0530

    Development (#1)
    * S1 - Added ClassTwo
    
    * S2 - Added new property

--- Program.cs
+++ Program.cs
@@ -7,5 +7,11 @@ namespace Premier.API
         public string PropertyOne { get; set; }
         public string PropertyTwo { get; set; } 
     }
+
+    public class ClassTwo
+    {
+        public string ID { get; set; }
+        public string Value { get; set; }
+    }
 }
     
